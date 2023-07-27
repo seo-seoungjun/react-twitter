4527e25529343cd10869b8526d8fc2e9eec6bd6a
seo-seoungjun <tjtjdwns1860@naver.com>
2023-07-27 18:33:47 +0900

    feat:Navigation bar
    메인 페이지와 프로필 페이지에 네비게이션 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,24 +2,35 @@ import {
   HashRouter as Router,
   Route,
   Switch,
+  Redirect,
 } from 'react-router-dom/cjs/react-router-dom.min';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
+import Nav from './Nav';
+import MyProfile from '../routes/Profile';
 
 const AppRouters = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Nav />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <MyProfile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
