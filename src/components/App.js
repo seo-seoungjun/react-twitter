3ca7553e6048e5f5bc1d6be5defd90e099585c72
seo-seoungjun <tjtjdwns1860@naver.com>
2023-07-27 22:11:14 +0900

    feat:tweet CRUD(#13)
    tweet 생성, 삭제, 수정 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,27 @@ import { authService } from '../myBase';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
-  return <>{init ? <AppRouters isLoggedIn={isLoggedIn} /> : 'initialize,,,'}</>;
+  return (
+    <>
+      {init ? (
+        <AppRouters isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        'initialize,,,'
+      )}
+    </>
+  );
 }
 
 export default App;
